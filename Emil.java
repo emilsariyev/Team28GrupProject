6497953104d0a9ff2ba32e6b10c297b410c6fe3c
Emil <esariyev20557@ada.edu.az>
2026-04-05 16:13:53 +0400

    Added Emil.java with print statement

--- Emil.java
+++ Emil.java
@@ -1,6 +1,5 @@
 public class Emil {
     public static void main(String[] args) {
-        System.out.println("Hello, Emil!");
-        System.out.println("Welcome to the Team 28 Group Project!");
+        System.out.println("Emil");
     }
 }
