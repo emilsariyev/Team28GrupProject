af1cfc9bcedb098064371d1f740ab22fbed3543e
Emil Sariyev <esariyev20557@ada.edu.az>
2026-04-06 00:14:59 +0400

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        System.out.println("pipitu");
-        System.out.println("pipiwan");
-        System.out.println("p");
-    }
-}
