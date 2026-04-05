fe2da521c29af10b2ea0407d1c241197fd1acc5a
Emil <esariyev20557@ada.edu.az>
2026-04-05 16:10:35 +0400

    Commmit1

--- Emil.java
+++ Emil.java
@@ -1,5 +1,6 @@
 public class Emil {
     public static void main(String[] args) {
         System.out.println("Hello, Emil!");
+        System.out.println("Welcome to the Team 28 Group Project!");
     }
 }
