dc8201879bca28cabfafb783eb79f5ab3f7cb44c
Mahsun Alkhasov <mahsunalkhasov@gmail.com>
2026-04-10 13:40:47 +0400

    Lol

--- Main.java
+++ Main.java
@@ -13,5 +13,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        System.out.println("Application started successfully.");
     }
 }
